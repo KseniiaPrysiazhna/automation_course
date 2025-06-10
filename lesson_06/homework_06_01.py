c2d51dadce1a7ca6c7de082591cc66f2cd3a4eb8
Kseniia <kseniia.prysiazhna@tabby.ai>
2025-06-10 20:49:26 +0300

    homework_06_01 with fixes

--- lesson_06/homework_06_01.py
+++ lesson_06/homework_06_01.py
@@ -1,6 +1,7 @@
+# Порахувати кількість унікальних символів в строці.
+# Якщо їх більше 10 - вивести в консоль True, інакше - False.
+# Строку отримати за допомогою функції input()
+
 text = input("Enter text: ")
-unique_chars = set(text)
-if len(unique_chars) > 10:
-    print("True")
-else:
-    print("False")
+
+print ((len(set(text)) > 10))
