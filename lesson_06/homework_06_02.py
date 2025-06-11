ff51cdbed782fd5ae5ec76ceb5af8926f8948519
Kseniia <kseniia.prysiazhna@tabby.ai>
2025-06-12 01:14:49 +0300

    fixed minor issue in homework_06_02

--- lesson_06/homework_06_02.py
+++ lesson_06/homework_06_02.py
@@ -2,7 +2,7 @@
 # в якому є літера "h" (враховуються як великі так і маленькі).
 # Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
 
-text = input("Enter text with 'h'': ")
+text = input("Enter text with 'h': ")
 while not 'h' in text.lower():
-    text = input("Enter text with 'h': ")
+    text = input("Please try again.Enter text with 'h': ")
 print("Accepted!")
